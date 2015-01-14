ae7178d0ada595f850d895a83e2c7b6e1180a1bc
jedireza <reza@akhavan.me>
2015-01-13 21:06:52 -0800

    updated google analytics id

--- js/google.analytics.js
+++ js/google.analytics.js
@@ -3,5 +3,5 @@
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', '{TODO REPLACE ME}', 'auto');
+ga('create', 'UA-58533984-1', 'auto');
 ga('send', 'pageview');
